a3cc0ab2f06a86f01e954f50b8c882eef443fb31
Amar Babuta <33984012+amarbabuta@users.noreply.github.com>
2024-10-26 21:15:21 +1100

    Update model_predict.py
    If no model exists the prediction shows please train the model.

--- customer-review/app/src/model_predict.py
+++ customer-review/app/src/model_predict.py
@@ -15,21 +15,25 @@ def model_predict(
         model = BertForSequenceClassification.from_pretrained('app/models/saved_model')
         tokenizer = BertTokenizer.from_pretrained('app/models/saved_model')
         model.eval()
-    try:    
-        # Tokenize the input review
-        inputs = tokenizer(review, return_tensors='pt', padding=True, truncation=True, max_length=512)
-        with torch.no_grad():
-            logits = model(**inputs).logits
-            prediction = torch.argmax(logits, dim=-1).item()
+        try:    
+            # Tokenize the input review
+            inputs = tokenizer(review, return_tensors='pt', padding=True, truncation=True, max_length=512)
+            with torch.no_grad():
+                logits = model(**inputs).logits
+                prediction = torch.argmax(logits, dim=-1).item()
+        
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
+
+
+        sentiment = 'positive' if prediction == 1 else 'negative'
+            
+            
+        # Get the predictions
+        outputs = ModelPredictOutput(sentiment=sentiment)
     
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Error during prediction: {str(e)}")
-
-
-    sentiment = 'positive' if prediction == 1 else 'negative'
-        
-        
-    # Get the predictions
-    outputs = ModelPredictOutput(sentiment=sentiment)
+    else:
+        # Get the predictions
+        outputs = ModelPredictOutput(sentiment='First train the model')
     return outputs
     
